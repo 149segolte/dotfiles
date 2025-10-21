2e2b625e69d6199b1689b841de785a8075ab7e22
149segolte <37300847+149segolte@users.noreply.github.com>
2025-10-21 00:55:46 -0400

    feat: add multi platform support

--- quay_manifest_update.py
+++ quay_manifest_update.py
@@ -13,11 +13,10 @@ import subprocess
 class Remote:
     name: str
     upstream: str
-    tag: str
     mirror: str
 
     def exists(self, mirror: bool = False) -> bool:
-        loc = self.upstream
+        loc = self.upstream.split(":")[0]
         if mirror:
             loc = self.mirror
 
@@ -29,50 +28,58 @@ class Remote:
             return False
 
         lines = out.stdout.decode().splitlines()
-        if len(lines) > 1 and lines[1].strip().startswith(loc):
-            print("ok.")
-            return True
+        if len(lines) < 2 or not lines[1].strip().startswith(loc):
+            print("not found (invalid output).")
+            return False
 
-        print("not found.")
-        return False
+        print("ok.")
+        return True
 
-    def fetch(self, mirror: str = "") -> str:
-        loc = f"{self.upstream}:{self.tag}"
-        if mirror != "":
-            loc = f"{self.mirror}:{mirror}"
+    def fetch(self, platform: str, mirror: bool = False) -> str:
+        loc = self.upstream
+        if mirror:
+            loc = self.mirror
 
         print("Fetching... ", end="")
 
-        out = subprocess.run(["podman", "image", "pull", loc], capture_output=True)
+        out = subprocess.run(
+            ["podman", "image", "pull", f"--platform={platform}", loc],
+            capture_output=True,
+        )
         if out.returncode != 0:
             print("failed.")
             return ""
 
         lines = out.stdout.decode().splitlines()
-        if len(lines) > 0:
-            hash = lines[-1].strip()
-            if len(hash) == 64:
-                print("ok.")
-                return hash
+        if len(lines) < 1:
+            print("failed (invalid output).")
+            return ""
 
-        print("failed.")
-        return ""
+        hash = lines[-1].strip()
+        if len(hash) != 64:
+            print("failed (invalid hash).")
+            return ""
 
-    def push(self, mirror: str) -> None:
-        loc = f"{self.mirror}:{mirror}"
+        print("ok.")
+        return hash
+
+    def push(self, platform: str) -> bool:
+        loc = f"{self.mirror}:{platform.split('/')[1]}"
+
         print("Pushing... ", end="")
 
         out = subprocess.run(
-            ["podman", "image", "tag", f"{self.upstream}:{self.tag}", loc],
+            ["podman", "image", "tag", self.upstream, loc],
             capture_output=True,
         )
         if out.returncode != 0:
             print("failed (could not tag image).")
-            return
+            return False
 
         lines = out.stdout.decode().splitlines()
         if len(lines) > 0:
             print("failed (tagging invalid output).")
+            return False
 
         out = subprocess.run(
             ["podman", "image", "push", "--tls-verify=false", loc],
@@ -80,41 +87,68 @@ class Remote:
         )
         if out.returncode != 0:
             print("failed (could not push image).")
-            return
+            return False
 
         lines = out.stderr.decode().splitlines()
         if (
-            len(lines) > 1
-            and lines[-1].strip() == "Writing manifest to image destination"
+            len(lines) < 2
+            or not lines[-1].strip() == "Writing manifest to image destination"
         ):
-            print("ok.")
-            return
+            print("failed (pushing invalid output).")
+            return False
 
-        print("failed.")
-        return
+        print("ok.")
+        return True
 
+    def commit(self, platforms: list[str]) -> bool:
+        print("Updating manifest... ", end="")
 
-remotes = [
-    Remote(
-        name="alpine",
-        upstream="docker.io/library/alpine",
-        tag="3.22",
-        mirror="quay.io/149segolte/alpine",
-    ),
-    Remote(
-        name="fedora",
-        upstream="quay.io/fedora/fedora",
-        tag="42",
-        mirror="quay.io/149segolte/fedora",
-    ),
-]
+        out = subprocess.run(
+            ["podman", "manifest", "create", f"{self.mirror}:latest"],
+            capture_output=True,
+        )
+        if out.returncode != 0:
+            print("failed (could not create manifest).")
+            return False
+
+        for platform in platforms:
+            loc = f"{self.mirror}:{platform.split('/')[1]}"
+            out = subprocess.run(
+                [
+                    "podman",
+                    "manifest",
+                    "add",
+                    f"{self.mirror}:latest",
+                    f"docker://{loc}",
+                ],
+                capture_output=True,
+            )
+            if out.returncode != 0:
+                print(f"failed (could not add {platform} to manifest).")
+                return False
+
+        out = subprocess.run(
+            ["podman", "manifest", "push", "--all", f"{self.mirror}:latest"],
+            capture_output=True,
+        )
+        if out.returncode != 0:
+            print("failed (could not push manifest).")
+            return False
+
+        print("ok.")
+        return True
 
 
 parser = ArgumentParser(
     prog="quay_manifest_update.py", description="Update Quay manifests"
 )
 parser.add_argument(
-    "push", choices=["latest", "beta"], help="Push changes to the remote repository"
+    "tag", choices=["latest", "beta"], help="Push changes to the remote repository"
+)
+parser.add_argument(
+    "--platform",
+    default="linux/arm64,linux/amd64",
+    help="Specify the platform(s) to build for",
 )
 
 
@@ -124,6 +158,24 @@ def separator() -> None:
 
 def main() -> None:
     args = parser.parse_args()
+    platforms = args.platform.split(",")
+    if len(platforms) == 0:
+        print("No platforms specified.")
+        return
+
+    remotes = [
+        Remote(
+            name="alpine",
+            upstream="docker.io/library/alpine:3.22",
+            mirror="quay.io/149segolte/alpine",
+        ),
+        Remote(
+            name="fedora",
+            upstream="quay.io/fedora/fedora:42",
+            mirror="quay.io/149segolte/fedora",
+        ),
+    ]
+
     print("Quay manifest update script")
     separator()
     print("Remotes: ", [r.name for r in remotes])
@@ -134,20 +186,25 @@ def main() -> None:
             print("Skipping.")
             continue
 
-        source_hash = remote.fetch()
-        if source_hash == "":
-            print("Failed to fetch upstream image.")
-            continue
+        for platform in platforms:
+            print(f"\nPlatform: {platform}")
+            source_hash = remote.fetch(platform)
+            if source_hash == "":
+                print("Failed to fetch upstream image.")
+                continue
 
-        mirror_hash = ""
-        if remote.exists(mirror=args.push):
-            mirror_hash = remote.fetch(mirror=args.push)
+            mirror_hash = ""
+            if remote.exists(mirror=True):
+                mirror_hash = remote.fetch(platform, mirror=True)
 
-        if source_hash == mirror_hash:
-            print("No update needed.")
-            continue
+            if source_hash == mirror_hash:
+                print(f"No update needed for {platform}.")
+                continue
 
-        remote.push(args.push)
+            remote.push(platform)
+
+        print(f"\nManifest for: {platforms}", end="")
+        remote.commit(platforms)
 
 
 if __name__ == "__main__":
