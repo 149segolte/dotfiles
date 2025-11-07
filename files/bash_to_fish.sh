ecaac03ac2d09d392fff939d6754b704a410ec0b
149segolte <37300847+149segolte@users.noreply.github.com>
2025-11-07 14:39:04 -0500

    feat: ensure fish before switch

--- files/bash_to_fish.sh
+++ files/bash_to_fish.sh
@@ -1,7 +1,9 @@
 
 # Switch to fish shell
-if [[ ( $PPID -eq 0 || $(ps --no-header --pid=$PPID --format=comm) != "fish" ) && -z ${BASH_EXECUTION_STRING} && ${SHLVL} == 1 && $- == *i* ]]
-then
-	shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=''
-	exec fish $LOGIN_OPTION
+if command -v fish >/dev/null 2>&1; then
+    if [[ ( $PPID -eq 0 || $(ps --no-header --pid=$PPID --format=comm) != "fish" ) && -z ${BASH_EXECUTION_STRING} && ${SHLVL} == 1 && $- == *i* ]]
+    then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=''
+        exec fish $LOGIN_OPTION
+    fi
 fi
