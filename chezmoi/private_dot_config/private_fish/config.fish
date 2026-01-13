e2e9b1298ed233ba6b64b95511386d628fe4a0e6
149segolte <37300847+149segolte@users.noreply.github.com>
2026-01-13 00:27:10 -0500

    feat: add fish config with plugins

---
+++ chezmoi/private_dot_config/private_fish/config.fish
@@ -0,0 +1,18 @@
+# Fisher Plugins
+fisher_check "jorgebucaran/fisher" "patrickf1/fzf.fish" "ilancosman/tide@v6"
+
+# Global config
+set -g fish_greeting
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    abbr -a -- cat 'bat --style=plain'
+    abbr -a -- cd 'z'
+    abbr -a -- grep 'rg'
+    abbr -a -- ll 'eza -la'
+    abbr -a -- ls 'eza'
+end
+
+# Zoxide
+zoxide init fish | source
+
